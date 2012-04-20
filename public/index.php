3a9b6047d1ccdb9881fcfb9e50e052cf62a8b615
Rafael <jrtadf@gmail.com>
2012-04-19 23:40:02 -0300

    Autoloader implementado e testado

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('APPLICATION_PATH')
+        || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'),
+            get_include_path(),
+        )));
+
+require_once 'MRCartas/Autoloader.php';
