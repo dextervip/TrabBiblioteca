979a3454dbdb844d49f4c8d7c733d57803ec0c97
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-05-29 13:22:58 -0300

    Refatorações....

--- app/index.php
+++ app/index.php
@@ -1,6 +1,7 @@
 <?php
 
 include '/../build/package.phar';
+
 /**
  * Description of index
  *
@@ -9,16 +10,14 @@ class index {
 
     private $listaCartas = array();
 
-   
-
     public function adicionarCartas() {
 
         $baralho = new \MRCartas\Baralho();
-        
-        $naipes = array('paus', 'ouro','espada','copas');
-        $valores = array('A','K','Q','J','10','9','8','7','6','5','4','3','2');
+
+        $naipes = array('paus', 'ouro', 'espada', 'copas');
+        $valores = array('A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2');
         foreach ($naipes as $naipe) {
-            foreach  ($valores as $value) {
+            foreach ($valores as $value) {
                 $carta = new \MRCartas\Carta();
                 $carta->setNaipe($naipe);
                 $carta->setValor($value);
@@ -27,39 +26,33 @@ class index {
         }
         echo "BARALHO --------------------------------------------------------";
         var_dump($baralho->getBaralho());
-        
+
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</br>";
-        
+
         echo "TIRANDO CARTA DO INICIO PASSANDO PARA O FIM";
         $baralho->passaCartaInicioFim();
         var_dump($baralho->getBaralho());
-        
+
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</br>";
         echo "RETIRANDO CARTA DO FIM DO BARALHO";
         $baralho->retiraCartaFim();
         var_dump($baralho->getBaralho());
-        
+
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</br>";
         echo "EMBARALHANDO A CARTA";
         $baralho->embaralhaCartas();
         var_dump($baralho->getBaralho());
-        
+
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</br>";
         echo "RETIRANDO CARTA DO TOPO DO BARALHO";
         $baralho->retiraCartaTopo();
         var_dump($baralho->getBaralho());
-        
+
         echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</br>";
         echo "DIVIDIR BARALHO EM 2";
         $baralho->divideBaralho();
         var_dump($baralho->getBaralho());
-      
-        
-       
     }
 
-    
 }
-
-
 
