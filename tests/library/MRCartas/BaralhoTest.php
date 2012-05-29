be05105026f897d999e319224c4a0a88cf21e72b
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-05-29 13:08:40 -0300

    testDivideBaralho()

--- tests/library/MRCartas/BaralhoTest.php
+++ tests/library/MRCartas/BaralhoTest.php
@@ -105,7 +105,7 @@ class BaralhoTest extends \PHPUnit_Framework_TestCase {
      * @author Juliano R
      */
     public function testDivideBaralho() {
-        // Remove the following lines when you implement this test.
+        // Em construção...
         $this->markTestIncomplete(
                 'This test has not been implemented yet.'
         );
