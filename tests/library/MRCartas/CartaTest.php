1ee7453a3efa4e083e1b5009b39cd06c96a91065
Helison <Helison@10.2.9.238>
2012-08-29 17:32:14 -0300

    Metodos testGetSetValor, testGetSetValorWithWrongValues e testSetValor escritos

--- tests/library/MRCartas/CartaTest.php
+++ tests/library/MRCartas/CartaTest.php
@@ -62,16 +62,72 @@ class CartaTest extends \PHPUnit_Framework_TestCase {
      * @covers MRCartas\Carta::getValor
      * @todo Implement testGetValor().
      */
-    public function testGetValor() {
-        // Remove the following lines when you implement this test.
-        $this->fail('Não implementado ainda');
+    public function testGetSetValor() {
+        $valores = array('A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2');
+        foreach ($valores as $value) {
+            /**
+             * Chama a variável 'object' que é um objeto da classe
+             * Carta.php, acessa o método 'setValor' 
+             * e define por paramêtro a string $value.
+             */
+            $this->object->setValor($value);
+
+            /**
+             * Testa se a string(valor) retornada pelo método invocado 'getValor()' é a mesma
+             * de $value.
+             */
+            $this->assertSame($this->object->getValor(), $value);
+        }
     }
+
+    public function testGetSetValorWithWrongValues() {
+        $valores = array('D','P', 'X', 'U', 'O', '88', '18', 'o47', '50', '941', '41', '41', '24', '4u23','876','P0X4','T35T3');
+        foreach ($valores as $value) {
+            /**
+             * Chama a variável 'object' que é um objeto da classe
+             * Carta.php, acessa o método 'setValor' 
+             * e define por paramêtro a string $value.
+             */
+            try{
+            $this->object->setValor($value);
+            
+            /**
+             * Testa se a string(valor) retornada pelo método invocado 'getValor()' é a mesma
+             * de $value.
+             */
+            $this->assert($this->object->getValor(), $value);
+            } catch (InvalidArgumentException $expected){
+                return;
+            }
+             $this->fail('Nao ocorreu execeção.');
+            }
+        }
+    
 
     /**
      * @covers MRCartas\Carta::setValor
      * @todo Implement testSetValor().
      */
     public function testSetValor() {
+        /**
+         * Atribui o valor 5 para uma variável chamada valor
+         */
+        $valor = "5";
+        /**
+         * Chama a variável 'object' que é um objeto da classe
+         * Carta.php, acessa o método 'setValor' 
+         * e define por paramêtro o atributo valor.
+         */
+        $this->object->setValor($valor);
+
+        /**
+         * Testa se a string(valor) retornada pelo método invocado 'getValor()'
+         * é o mesmo que se encontra na variável valor.
+         */
+        $this->assertSame($this->object->getValor(), $valor);
+
+
+
         // Remove the following lines when you implement this test.
         $this->fail('Não implementado ainda');
     }
