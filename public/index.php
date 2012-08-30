0bcafe46b2e5ef9a58a0e69bf5ad91a5fc0683af
Rafael <jrtadf@gmail.com>
2012-08-30 00:57:58 -0300

    correção include path

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ $url = '/' . substr_replace($url_request, '', 0, strlen(APPLICATION_PATH));
 
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../'));
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../library'));
 require_once '/../library/MRCartas/Autoloader.php';
 require_once 'App/JogoApp.php';
 require_once 'App/Model/Jogo.php';
@@ -16,7 +17,7 @@ require_once 'App/Model/Jogador.php';
 
 $jogoApp = new \App\JogoApp();
 
-require_once '/../library/Zend/Session.php';
+require_once 'Zend/Session/Namespace.php';
 $session = new Zend_Session_Namespace();
 if (isset($session->jogo) === false) {
     $session->jogo = new \App\Model\Jogo();
