c5e622bc0d9ee2ea3b823c446a9470468bca2563
Rafael <jrtadf@gmail.com>
2012-08-30 01:57:45 -0300

    jogo.html movido para App/View/Home.php

--- public/jogo.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once '/../app/Jogo.php';
-$jogo = new Jogo();
---
+++ public/js/jogo.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+  
+    $("#botao-proxima-carta").click(function(){
+        alert('voce clicou em proxima carta');
+    });
+
+});
+
+
