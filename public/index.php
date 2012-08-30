9dc1f0d5461f0e057c831c4133e984324b6d8027
Rafael <jrtadf@gmail.com>
2012-08-30 01:11:44 -0300

    Adicionado rota pegar carta

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ $url = '/' . substr_replace($url_request, '', 0, strlen(APPLICATION_PATH));
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../'));
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../library'));
-require_once '/../library/MRCartas/Autoloader.php';
+require_once 'MRCartas/Autoloader.php';
 require_once 'App/JogoApp.php';
 require_once 'App/Model/Jogo.php';
 require_once 'App/Model/Jogador.php';
@@ -18,15 +18,14 @@ require_once 'App/Model/Jogador.php';
 $jogoApp = new \App\JogoApp();
 
 require_once 'Zend/Session/Namespace.php';
+require_once 'Zend/Json/Encoder.php';
 $session = new Zend_Session_Namespace();
 if (isset($session->jogo) === false) {
     $session->jogo = new \App\Model\Jogo();
 }
 
 
-$jogoApp->get('/', function () use($session){
-            var_dump($session->jogo->getCarta());
-            die;
+$jogoApp->get('/', function () use($session) {
             require '../App/View/Home.php';
         });
 
@@ -34,5 +33,9 @@ $jogoApp->get('/oi', function () {
             echo 'helloooo mano!';
         });
 
+$jogoApp->get('/pegar-carta', function () use($session) {
+            $carta = $session->jogo->getCarta();
+            echo Zend_Json_Encoder::encode(array('naipe'=>$carta->getNaipe(),'valor'=>$carta->getValor()));
+        });
 
 $jogoApp->run($url);
